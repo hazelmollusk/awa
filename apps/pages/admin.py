85419a9a5820aa453acafc8439a1af403b40e385
Matt Barry <matt@hazelmollusk.org>
2025-08-22 23:34:50 -0400

    .

--- apps/pages/admin.py
+++ apps/pages/admin.py
@@ -17,25 +17,25 @@ class PageSectionImageAdmin(NestedStackedInline):
     # fieldsets = [
     #     (None, ('title', 'image')),
     # ]
-    fields = ('title','image')
+    fields = ("title", "image")
     extra = 0
     min_num = 0
 
+
 class PageSectionAdminForm(forms.ModelForm):
     model = PageSection
-    fields = '__all__'
-    widgets = {
-        'content': SummernoteTextField()
-    }
+    fields = "__all__"
+    widgets = {"content": SummernoteTextField()}
+
 
 class PageSectionAdmin(NestedStackedInline):
     model = PageSection
     min_num = 0
     max_num = 20
     extra = 1
-    fields = ("title","draft","content")
+    fields = ("title", "draft", "content")
     # fieldsets = [
-    #     (None, ("title", "draft")), 
+    #     (None, ("title", "draft")),
     #     (None, ("content",))
     # ]
     inlines = [PageSectionImageAdmin]
@@ -45,6 +45,7 @@ class PageSectionAdmin(NestedStackedInline):
 class PageAdmin(NestedModelAdmin, GuardedModelAdminMixin):
     readonly_fields = AUDIT_FIELDS
     prepopulated_fields = {"slug": ["title"]}
+    list_display = ("title", "slug", "draft", "visible")
     fieldsets = [
         (None, {"fields": ["title", "slug", "draft", "visible"]}),
         ("Audit", {"fields": AUDIT_FIELDS}),
